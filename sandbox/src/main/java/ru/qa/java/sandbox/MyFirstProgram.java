4cde4da6073a7ab4931880571fcece2d7c700fef
PetrBorisenok <borisenokpf@gmail.com>
2017-12-22 20:51:16 +0300

    Added several functions

--- sandbox/src/main/java/ru/qa/java/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/qa/java/sandbox/MyFirstProgram.java
@@ -3,6 +3,27 @@ package ru.qa.java.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello world !");
+        hello("world");
+        hello("Pep");
+        hello("Arsen");
+
+        double k = 6;
+        System.out.println("Площадь квадрата со стороной " + k + " = " + area(k));
+
+        double p1 = 11;
+        double p2 = 14;
+        System.out.println("Площадь прямоугольника со сторонами " + p1 + " и " + p2 + " = " + area(p1, p2));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello " + somebody + " !");
+    }
+
+    public static double area(double a){
+        return a * a;
+    }
+
+    public static double area(double a, double b){
+        return a * b;
     }
 }

a1c430d2368ad5fe12ab840a4c39085ffb22fca0
PetrBorisenok <borisenokpf@gmail.com>
2017-12-23 13:37:15 +0300

    Added distance method, release Task№2

--- sandbox/src/main/java/ru/qa/java/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/qa/java/sandbox/MyFirstProgram.java
@@ -13,21 +13,19 @@ public class MyFirstProgram {
 //        Rectangle r = new Rectangle(2,5);
 //        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-        Point p1 = new Point();
-        p1.x1 = 6;
-        p1.y1 = 10;
-
-        Point p2 = new Point();
-        p2.x2 = 3;
-        p2.y2 = -5;
-        System.out.println("Расстояние между двумя точками " + "(" + p1.x1 + ";" + p1.y1 + ")" + " и " +
-                                                                "(" + p2.x2 + ";" + p2.y2 + ")" + " = " + distance(p1, p2));
+        Point p1 = new Point(6, 10);
+        Point p2 = new Point(3, -5);
+        System.out.println("Расстояние между двумя точками " + "(" + p1.x + ";" + p1.y + ")" + " и " +
+                                                                "(" + p2.x + ";" + p2.y + ")" + " = " + p1.distance(p2));
+        Point p3 = new Point(4,5);
+        Point p4 = new Point(8,10);
+        System.out.println("Расстояние между двумя точками " + "(" + p3.x + ";" + p3.y + ")" + " и " +
+                                                                "(" + p4.x + ";" + p4.y + ")" + " = " + p3.distance(p4));
+        Point p5 = new Point(0,-4);
+        Point p6 = new Point(4,9);
+        System.out.println("Расстояние между двумя точками " + "(" + p5.x + ";" + p5.y + ")" + " и " +
+                                                                "(" + p6.x + ";" + p6.y + ")" + " = " + p5.distance(p6));
     }
-
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt( Math.pow(p2.x2 - p1.x1,2) + Math.pow(p2.y2 - p1.y1,2) );
-    }
-
 //    public static void hello(String somebody) {
 //        System.out.println("Hello " + somebody + " !");
 //    }
--- sandbox/src/main/java/ru/qa/java/sandbox/Point.java
+++ sandbox/src/main/java/ru/qa/java/sandbox/Point.java
@@ -2,10 +2,15 @@ package ru.qa.java.sandbox;
 
 public class Point {
 
-  public double x1;
-  public double y1;
+  public double x;
+  public double y;
 
-  public double x2;
-  public double y2;
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
 
+  public double distance(Point p) {
+    return Math.sqrt( Math.pow(this.x - p.x,2) + Math.pow(this.y - p.y,2) );
+  }
 }

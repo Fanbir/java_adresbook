2cd3a1986c45bcb61415307a122fdd52c36453b7
PetrBorisenok <borisenokpf@gmail.com>
2017-12-29 15:26:53 +0300

    Refactor EqualitionTest

--- sandbox/src/test/java/ru/qa/java/sandbox/EquationTest.java
+++ sandbox/src/test/java/ru/qa/java/sandbox/EquationTest.java
@@ -19,4 +19,16 @@ public class EquationTest {
     Equation  e = new Equation(1, 5, 6);
     Assert.assertEquals(e.rootNumber(), 2) ;
   }
+  public void testLiniar() {
+    Equation  e = new Equation(0, 1, 1);
+    Assert.assertEquals(e.rootNumber(), 1) ;
+  }
+  public void testConstant() {
+    Equation  e = new Equation(0, 0, 1);
+    Assert.assertEquals(e.rootNumber(), 0) ;
+  }
+  public void testZero() {
+    Equation  e = new Equation(0, 0, 0);
+    Assert.assertEquals(e.rootNumber(), -1) ;
+  }
 }

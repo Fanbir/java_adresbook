9cd77a430fc8c17f3641f9a909ed0ae786475e82
PetrBorisenok <borisenokpf@gmail.com>
2017-12-30 00:04:51 +0300

    Optimized NavigationForm

--- addressbook-web-tests/src/test/java/ru/qa/java/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/qa/java/addressbook/appmanager/NavigationHelper.java
@@ -11,22 +11,42 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void gotoGroupPage() {
+    if( isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+       return;
+    }
     click(By.linkText("groups"));
   }
 
   public void gotoAddContact() {
-   click(By.linkText("add new"));
+    if( isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")) {
+      return;
+    }
+    click(By.linkText("add new"));
   }
 
   public void returnHomePage() {
-   click(By.linkText("home"));
+    if(isElementPresent(By.id("maintable"))) {
+      return;
+    }
+    click(By.linkText("home"));
   }
 
   public void gotoHomePage() {
+    if(isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.xpath(".//*[@id='nav']/ul/li[1]/a"));
   }
 
   public void returnGroupPage() {
+    if( isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 }

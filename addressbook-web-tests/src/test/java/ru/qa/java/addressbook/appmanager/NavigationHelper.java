ae62ac6c1e8c7528c4b3700085afbc676cc0bec6
PetrBorisenok <borisenokpf@gmail.com>
2017-12-30 04:28:46 +0300

    Refactoring NavigationHelper

--- addressbook-web-tests/src/test/java/ru/qa/java/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/qa/java/addressbook/appmanager/NavigationHelper.java
@@ -11,42 +11,22 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void gotoGroupPage() {
-    if( isElementPresent(By.tagName("h1"))
-            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
-            && isElementPresent(By.name("new"))) {
-       return;
-    }
     click(By.linkText("groups"));
   }
 
   public void gotoAddContact() {
-    if( isElementPresent(By.tagName("h1"))
-            && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")) {
-      return;
-    }
-    click(By.linkText("add new"));
+   click(By.linkText("add new"));
   }
 
   public void returnHomePage() {
-    if(isElementPresent(By.id("maintable"))) {
-      return;
-    }
-    click(By.linkText("home"));
+   click(By.linkText("home"));
   }
 
   public void gotoHomePage() {
-    if(isElementPresent(By.id("maintable"))) {
-      return;
-    }
     click(By.xpath(".//*[@id='nav']/ul/li[1]/a"));
   }
 
   public void returnGroupPage() {
-    if( isElementPresent(By.tagName("h1"))
-            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
-            && isElementPresent(By.name("new"))) {
-      return;
-    }
     click(By.linkText("groups"));
   }
 }

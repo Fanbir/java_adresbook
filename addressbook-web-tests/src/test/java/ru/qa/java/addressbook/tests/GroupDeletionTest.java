457d16e80aeee570f13694b64ccc3e7a93eee979
PetrBorisenok <borisenokpf@gmail.com>
2018-01-01 18:38:55 +0300

    Creat Collections Group

--- addressbook-web-tests/src/test/java/ru/qa/java/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/qa/java/addressbook/tests/GroupDeletionTest.java
@@ -4,21 +4,23 @@ import org.junit.Assert;
 import org.testng.annotations.Test;
 import ru.qa.java.addressbook.model.GroupDate;
 
+import java.util.List;
+
 public class GroupDeletionTest extends TestBase {
 
 
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if(! app.getGroupHelper().isThereAGroup()) {
           app.getGroupHelper().createGroup(new GroupDate("test1",null, null));
     }
-    app.getGroupHelper().selectGroup(before -1);
+    List<GroupDate> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().deletedSelectedGroup();
     app.getGroupHelper().returnGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before - 1);
+    List<GroupDate> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() -1);
   }
 
 }

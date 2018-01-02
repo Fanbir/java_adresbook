68e236f1a76e5aa713aa1ae9c8a4adcf476c9563
PetrBorisenok <borisenokpf@gmail.com>
2018-01-02 15:34:32 +0300

    Refactror Tests, Added Equals Collections

--- addressbook-web-tests/src/test/java/ru/qa/java/addressbook/model/GroupDate.java
+++ addressbook-web-tests/src/test/java/ru/qa/java/addressbook/model/GroupDate.java
@@ -2,15 +2,53 @@ package ru.qa.java.addressbook.model;
 
 public class GroupDate {
   private final String name;
+  private final String id;
   private final String header;
   private final String footer;
 
-  public GroupDate(String name, String header, String footer) {
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupDate groupDate = (GroupDate) o;
+
+    if (name != null ? !name.equals(groupDate.name) : groupDate.name != null) return false;
+    return id != null ? id.equals(groupDate.id) : groupDate.id == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = name != null ? name.hashCode() : 0;
+    result = 31 * result + (id != null ? id.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupDate{" +
+            "name='" + name + '\'' +
+            ", id='" + id + '\'' +
+            '}';
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public GroupDate(String id, String name, String header, String footer) {
     this.name = name;
+    this.id = id;
     this.header = header;
     this.footer = footer;
   }
 
+  public GroupDate( String name, String header, String footer) {
+    this.name = name;
+    this.id = null;
+    this.header = header;
+    this.footer = footer;
+  }
   public String getName() {
     return name;
   }
--- addressbook-web-tests/src/test/java/ru/qa/java/addressbook/tests/GroupModoficationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/java/addressbook/tests/GroupModoficationTests.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.testng.annotations.Test;
 import ru.qa.java.addressbook.model.GroupDate;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModoficationTests extends TestBase {
@@ -17,10 +18,16 @@ public class GroupModoficationTests extends TestBase {
     List<GroupDate> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupDate("test1", "test2", "test3"));
+    GroupDate group = new GroupDate(before.get(before.size()-1).getId(),"test1", "test2", "test3");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnGroupPage();
     List<GroupDate> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() -1 );
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
   }
 }

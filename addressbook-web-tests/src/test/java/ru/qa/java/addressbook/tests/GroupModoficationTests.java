92c85725106ea6902c16b571ec5fdd4ecee8db22
PetrBorisenok <borisenokpf@gmail.com>
2018-01-10 22:06:48 +0300

    Hashing GroupTest

--- addressbook-web-tests/src/test/java/ru/qa/java/addressbook/tests/GroupModoficationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/java/addressbook/tests/GroupModoficationTests.java
@@ -30,8 +30,8 @@ public class GroupModoficationTests extends TestBase {
     GroupDate group = new GroupDate()
             .withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
     app.group().modify(group);
+    assertThat(app.group().сount(), equalTo(before.size()));
     Groups after = app.group().all();
-    assertEquals(after.size(), before.size());
     assertThat(after, equalTo(before.withOut(modifiedGroup).withAdded(group)));
 
   }

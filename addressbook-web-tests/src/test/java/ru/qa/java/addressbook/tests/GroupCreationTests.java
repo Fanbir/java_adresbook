18378ac0abaf952c5016c5df2273454fe5c11d99
PetrBorisenok <borisenokpf@gmail.com>
2018-01-12 02:09:02 +0300

    Added  @DataProvider GroupCreationTest

--- addressbook-web-tests/src/test/java/ru/qa/java/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/java/addressbook/tests/GroupCreationTests.java
@@ -1,19 +1,33 @@
 package ru.qa.java.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.qa.java.addressbook.model.GroupDate;
 import ru.qa.java.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class GroupCreationTests extends TestBase{
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupDate().withName("test1").withHeader("header1").withFooter("footer1")});
+    list.add(new Object[] {new GroupDate().withName("test2").withHeader("header2").withFooter("footer2")});
+    list.add(new Object[] {new GroupDate().withName("test3").withHeader("header3").withFooter("footer3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupDate group) {
     app.goTo().GroupPage();
     Groups before = app.group().all();
-    GroupDate group = new GroupDate().withName("test2");
     app.group().create(group);
     assertThat(app.group().сount(), equalTo(before.size() + 1));
     Groups after = app.group().all();
@@ -21,14 +35,14 @@ public class GroupCreationTests extends TestBase{
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
-  @Test
-  public void testBadGroupCreation() {
-    app.goTo().GroupPage();
-    Groups before = app.group().all();
-    GroupDate group = new GroupDate().withName("test2'");
-    app.group().create(group);
-    assertThat(app.group().сount(), equalTo(before.size()));
-    Groups after = app.group().all();
-    assertThat(after, equalTo(before));
-  }
+//  @Test
+//  public void testBadGroupCreation() {
+//    app.goTo().GroupPage();
+//    Groups before = app.group().all();
+//    GroupDate group = new GroupDate().withName("test2'");
+//    app.group().create(group);
+//    assertThat(app.group().сount(), equalTo(before.size()));
+//    Groups after = app.group().all();
+//    assertThat(after, equalTo(before));
+//  }
 }
